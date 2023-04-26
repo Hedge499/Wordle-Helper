97dc3f701b4ee105d1b5b03c959969665b274cf5
Fred <frederik.buerschgens@gmail.com>
2023-04-26 21:11:44 +0200

    finish list code
    added trim function; implement trim, loadWordlistFromFile and createLetterCount

--- list.cpp
+++ list.cpp
@@ -1,13 +1,42 @@
 #include "list.hpp"
+#include <fstream>
+#include <regex>
 
+// trims whitespace from the beginning and end of str
+std::string trim(std::string str)
+{
+    return std::regex_replace(str, std::regex("^\\s+ | \\s+$"), std::string{""});
+}
+
+// read the wordlist.txt from the resource folder line by line.
+// returns a List of words.
 std::list<std::string> loadWordlistFromFile()
 {
     std::list<std::string> wordlist{};
+
+    std::ifstream wordlistFile("resource/wordlist.txt");
+    std::string line{};
+    while (std::getline(wordlistFile, line))
+    {
+        wordlist.push_back(trim(line));
+    }
+
     return wordlist;
 }
 
-std::map<char, int> createLetterCount(std::list<std::string>)
+// iterates through a list of words and counts the letters.
+// returns a map with a letter as key and the count as value.
+std::map<char, int> createLetterCount(std::list<std::string> wordlist)
 {
     std::map<char, int> letterCount{};
+
+    for (std::string str : wordlist)
+    {
+        for (char c : str)
+        {
+            letterCount[c]++;
+        }
+    }
+
     return letterCount;
 }
