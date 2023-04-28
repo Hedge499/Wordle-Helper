38869ab48691fc18ce0d9d1e3499c9b3d12f73a2
Fred <frederik.buerschgens@gmail.com>
2023-04-28 22:39:03 +0200

    add  user interaction

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "list.hpp"
+#include "wordle.hpp"
 #include <list>
 #include <string>
 #include <iostream>
@@ -26,7 +27,7 @@ std::ostream &operator<<(std::ostream &os, const std::map<char, int> &map)
 {
     for (auto const &[key, val] : map)
     {
-        os << key << " " << val << "\n";
+        os << "Letter " << key << " at the position " << val << ".\n";
     }
     return os;
 }
@@ -35,14 +36,14 @@ int main()
 {
     std::list<std::string> wordlist{loadWordlistFromFile()};
     std::map<char, int> lettersWithPosition{{'a', 2}, {'b', 3}};
-    std::list<char> lettersWithoutPosition{'d', 'e'};
+    std::list<char> lettersWithoutPosition{'i', 'o'};
     std::list<char> lettersNotInWord{'s'};
     bool running{true};
 
     while (running)
     {
         std::cout << "Known information:\n"
-                  << "Letters that are in the word and their Position:\n"
+                  << "Letters that are in the word and their Position(0-4):\n"
                   << lettersWithPosition
                   << "Letters that are in the word without Position:\n"
                   << lettersWithoutPosition
@@ -53,22 +54,34 @@ int main()
                   << "1) show potential answers\n"
                   << "2) show good next words\n"
                   << "3) add new information\n"
-                  << "4) exit\n"
-                  << "Choose by entering a number: ";
+                  << "4) erase information\n"
+                  << "5) exit\n"
+                  << "Choose by entering a number(1-5): ";
 
         int choice{0};
         std::cin >> choice;
         switch (choice)
         {
         case 1:
-
+            std::cout << getAnswer(lettersWithPosition, lettersWithoutPosition, lettersNotInWord, wordlist) << "\n";
             break;
-
+        case 2:
+            std::cout << nextWords(lettersWithPosition, lettersWithoutPosition, lettersNotInWord, wordlist) << "\n";
+            break;
+        case 3:
+            break;
+        case 4:
+            lettersWithPosition.clear();
+            lettersWithoutPosition.clear();
+            lettersNotInWord.clear();
+            break;
+        case 5:
+            running = false;
+            break;
         default:
+            std::cout << "Not a valid input!!!\n";
             break;
         }
-
-        running = false;
     }
 
     return 0;
