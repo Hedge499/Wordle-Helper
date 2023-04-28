fdc4971a3a2aeaad36b3b936a1e019eabb8ab6ff
Fred <frederik.buerschgens@gmail.com>
2023-04-28 22:09:30 +0200

    implement nextWord()

--- wordle.cpp
+++ wordle.cpp
@@ -1,4 +1,5 @@
 #include "wordle.hpp"
+#include "list.hpp"
 #include <algorithm>
 
 // creates a list of answers for a wordle round based on the given information
@@ -10,7 +11,7 @@ std::list<std::string> getAnswer(std::map<char, int> lettersWithPosition, std::l
     std::list<std::string> answers{};
 
     // filter out words that can not be the answer
-    for (std::string const word : wordlist)
+    for (std::string const &word : wordlist)
     {
         // is the word still viable as an answer
         bool fit{true};
@@ -18,7 +19,7 @@ std::list<std::string> getAnswer(std::map<char, int> lettersWithPosition, std::l
         // check wether the word has the right letters at the right position
         for (auto const &[letter, pos] : lettersWithPosition)
         {
-            if (word[pos] != letter)
+            if (word.at((long long unsigned int)pos) != letter)
             {
                 fit = false;
                 break;
@@ -71,4 +72,56 @@ std::list<std::string> getAnswer(std::map<char, int> lettersWithPosition, std::l
 // returns a list of 3 good next words
 std::list<std::string> nextWords(std::map<char, int> lettersWithPosition, std::list<char> lettersWithoutPosition, std::list<char> lettersNotInWord, std::list<std::string> wordlist)
 {
+    // values for each letter
+    std::map<char, int> letterValues{createLetterCount(wordlist)};
+
+    // the three words with the highest value
+    std::map<std::string, int> topWords{};
+
+    // determine the value of each word and check if it is a current top 3
+    for (auto const &word : wordlist)
+    {
+        int value{0};
+
+        // calculate value
+        for (auto const &letter : word)
+        {
+            if (lettersWithPosition.find(letter) == lettersWithPosition.end() && std::find(lettersNotInWord.begin(), lettersNotInWord.end(), letter) == lettersNotInWord.end())
+            {
+                if (std::find(lettersWithoutPosition.begin(), lettersWithoutPosition.end(), letter) != lettersWithoutPosition.end())
+                {
+                    value += (0.5 * letterValues[letter]);
+                }
+                else
+                {
+                    value += letterValues[letter];
+                }
+            }
+        }
+
+        // is the value bigger than the value of the words already in topWords
+        topWords[word] = value;
+
+        while (topWords.size() > 3)
+        {
+            std::pair<std::string, int> smallest{"", INT_MAX};
+            for (auto const &[word, value] : topWords)
+            {
+                if (value < smallest.second)
+                {
+                    smallest = {word, value};
+                }
+            }
+            topWords.erase(word);
+        }
+    }
+
+    // map to list
+    std::list<std::string> topThree{};
+    for (auto [word, value] : topWords)
+    {
+        topThree.push_back(word);
+    }
+
+    return topThree;
 }
