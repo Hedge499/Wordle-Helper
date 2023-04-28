371e3895c1f4e23dfcbe7e0b9860d12609c8c184
Fred <frederik.buerschgens@gmail.com>
2023-04-28 22:39:48 +0200

    add parameter names to header file wordle.hpp

--- wordle.hpp
+++ wordle.hpp
@@ -5,7 +5,7 @@
 #include <list>
 #include <string>
 
-std::list<std::string> getAnswer(std::map<char, int>, std::list<char>, std::list<char>, std::list<std::string>);
-std::list<std::string> nextWords(std::map<char, int>, std::list<char>, std::list<char>, std::list<std::string>);
+std::list<std::string> getAnswer(std::map<char, int> lettersWithPosition, std::list<char> lettersWithoutPosition, std::list<char> lettersNotInWord, std::list<std::string> wordlist);
+std::list<std::string> nextWords(std::map<char, int> lettersWithPosition, std::list<char> lettersWithoutPosition, std::list<char> lettersNotInWord, std::list<std::string> wordlist);
 
 #endif
